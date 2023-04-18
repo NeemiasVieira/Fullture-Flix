00bdda932e5eb82ed2e6d2752e6a7e80402bc382
Neemias Vieira <neemias.vieira@live.com>
2023-04-18 14:08:23 -0300

    Updates

---
+++ precache-manifest.2a0fb0bd9ff12b1646ad545f76a435a3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cde72236955862dd65f7",
+    "url": "/Fullture-Flix/static/css/main.a584e4ad.chunk.css"
+  },
+  {
+    "revision": "cde72236955862dd65f7",
+    "url": "/Fullture-Flix/static/js/main.a44cc376.chunk.js"
+  },
+  {
+    "revision": "25caf6469f12efa517fd",
+    "url": "/Fullture-Flix/static/js/runtime~main.cab73539.js"
+  },
+  {
+    "revision": "0c0142f0d4eaef776bc2",
+    "url": "/Fullture-Flix/static/js/2.12dfd73e.chunk.js"
+  },
+  {
+    "revision": "d119368b055b7c1018c8ce9ff355ea64",
+    "url": "/Fullture-Flix/static/media/fullflix.d119368b.jpeg"
+  },
+  {
+    "revision": "e9a4dd50724ac328bdb0792d2d606268",
+    "url": "/Fullture-Flix/static/media/error401.e9a4dd50.png"
+  },
+  {
+    "revision": "92d6ec3daa5cfbe425cb182e9b52e081",
+    "url": "/Fullture-Flix/index.html"
+  }
+];
